be6b6f0420914da943d82194c0caa29028b21d99
Shashwat Sharma <goto.shashwat@gmail.com>
2026-09-05 16:52:24 +0530

    ApexWallet project setup

---
+++ src/main/java/com/app/apexwallet/ApexWalletApplication.java
@@ -0,0 +1,13 @@
+package com.app.apexwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApexWalletApplication.class, args);
+	}
+
+}
